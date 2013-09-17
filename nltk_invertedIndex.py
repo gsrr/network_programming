d2d6d44cd410fce926f44337999aad03091b94c3
gsrr <jerrycheng1128@gmail.com>
2013-09-17 11:35:42 +0800

    update inverted index

--- nltk_invertedIndex.py
+++ nltk_invertedIndex.py
@@ -5,8 +5,6 @@ import string
 from nltk.stem.porter import PorterStemmer
 from nltk.tokenize import word_tokenize
 
-def outputIndexFile(index_content , output_path):
-    pass
 
 def combineTokens(index_content , tokens , file_id):
     for token in tokens.keys():
@@ -43,23 +41,52 @@ def getTokens(line):
     tokens = data_token(line_clean)
     tokens_stem = data_stem(tokens)
     return tokens_stem
-    
-def createInvertedIndex(path , file_id):
+
+def getIndexContent(index_path , idList):
     index_content = {}
-    if os.path.exists(output_path):
-        index_content = getIndexContent(output_path)
-        
+    fr = open(index_path , 'r')
+    while True:
+        line = fr.readline()
+        if not line: break
+        key = line.split()[0]
+        file_num = int(line.split()[1])
+        file_dic = {}
+        for i in range(0 , file_num):
+            file_line = fr.readline()
+            file_id = file_line.split()[0]
+            if file_id in idList:
+                file_dic[file_id] = 0
+            else:
+                file_dic[file_id] = int(file_line.split()[1])
+        index_content[key] = file_dic
+    return index_content
+    
+
+def createInvertedIndex(path , file_id , index_content):
     fd = open(path , 'r')
     for line in fd.readlines():
         tokens = getTokens(line)
         index_content = combineTokens(index_content , tokens , file_id)
     fd.close()
-    print index_content
-    
+    return index_content
+
+def outputIndexFile(index_content , output_path):
+    fw = open(output_path , 'w')
+    for key in index_content.keys():
+        file_list = index_content[key]
+        fw.write(key + " " + str(len(file_list)) + "\n")
+        for id in file_list.keys():
+            fw.write(id + " " + str(file_list[id]) + "\n")
+    fw.close()
+
 if __name__ == "__main__":
     path = "C:\\Python27\\test.txt"
     output_path = "C:\\Python27\\invertedIndex"
     file_id = "1"
-    index_content = createInvertedIndex(path , file_id)
+    index_content = {}
+    if os.path.exists(output_path):
+        index_content = getIndexContent(output_path , file_id)
+    
+    index_content = createInvertedIndex(path , file_id , index_content)
     outputIndexFile(index_content , output_path)
     
